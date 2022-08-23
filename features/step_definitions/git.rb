5007e56d4ac1f940df233c46116d02d632c70383
M. Scott Ford <scott@corgibytes.com>
2022-08-23 10:00:22 -0400

    Fixes linter warnings
    * Adds frozen literal declaration
    * Uses single quote strings when interpolation is not being used

--- features/step_definitions/git.rb
+++ features/step_definitions/git.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 require 'fileutils'
 
 Given('I clone the git repository {string} with the sha {string}') do |repository_url, sha|
   repositories_dir = "#{Aruba.config.working_directory}/tmp/repositories"
-  cloned_dir = "#{repositories_dir}/#{repository_url.split("/").last}"
+  cloned_dir = "#{repositories_dir}/#{repository_url.split('/').last}"
 
   FileUtils.mkdir_p(repositories_dir)
 
-  unless Dir.exists?(cloned_dir)
+  unless Dir.exist?(cloned_dir)
     $stdout.print "Cloning #{repository_url} ..."
-    unless system("git clone #{repository_url}", chdir: repositories_dir, out: "/dev/null", err: "/dev/null")
+    unless system("git clone #{repository_url}", chdir: repositories_dir, out: '/dev/null', err: '/dev/null')
       raise "Failed to clone #{repository_url}"
     end
-    puts "done."
+
+    puts 'done.'
   end
-  unless system("git checkout #{sha}", chdir: cloned_dir, out: "/dev/null", err: "/dev/null")
+  unless system("git checkout #{sha}", chdir: cloned_dir, out: '/dev/null', err: '/dev/null')
     raise "Failed to checkout #{sha}"
   end
 end
