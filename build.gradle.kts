9e844720d75f16903f3a686f50a9f475dd681b94
M. Scott Ford <scott@corgibytes.com>
2022-12-31 15:32:44 -0500

    Configures gradle to generate classes from proto file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,14 @@
+import com.google.protobuf.gradle.generateProtoTasks
+import com.google.protobuf.gradle.id
+import com.google.protobuf.gradle.plugins
+import com.google.protobuf.gradle.protobuf
+import com.google.protobuf.gradle.protoc
+import org.gradle.kotlin.dsl.invoke
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.22"
+    id("com.google.protobuf") version "0.8.18"
     application
 }
 
@@ -12,13 +19,24 @@ repositories {
     mavenCentral()
 }
 
+val grpcVersion: String by project
+val protobufVersion: String by project
+val grpcKotlinVersion: String by project
+
 dependencies {
+
     implementation("com.github.ajalt.clikt:clikt:3.5.0")
     implementation("com.corgibytes:dependency-history-maven:2.0.21")
     implementation("com.github.package-url:packageurl-java:1.4.1")
     implementation("org.apache.maven:maven-model:3.8.6")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("com.github.pgreze:kotlin-process:1.4.1")
+
+    api("io.grpc:grpc-stub:$grpcVersion")
+    api("io.grpc:grpc-protobuf:$grpcVersion")
+    api("com.google.protobuf:protobuf-java-util:$protobufVersion")
+    api("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+    api("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
 
     testImplementation(kotlin("test"))
 }
@@ -33,4 +51,29 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("com.corgibytes.freshli.agent.java.MainKt")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
