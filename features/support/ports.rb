cd4d9d875acfb661ebf983364e98a167cf09531e
M. Scott Ford <scott@corgibytes.com>
2023-01-09 11:57:39 -0500

    Waits until the port is released

--- features/support/ports.rb
+++ features/support/ports.rb
@@ -5,12 +5,19 @@ module Ports
   def self.available?(port)
     # based on https://stackoverflow.com/a/34375147/243215
     require 'socket'
+    max_attempts = 100
+    attempts = 0
     begin
+      attempts += 1
       socket = Socket.new(Socket::Constants::AF_INET, Socket::Constants::SOCK_STREAM, 0)
       socket.bind(Socket.pack_sockaddr_in(port, '0.0.0.0'))
       socket.close
       true
     rescue Errno::EADDRINUSE
+      if attempts < max_attempts
+        sleep 0.1
+        retry
+      end
       false
     end
   end
