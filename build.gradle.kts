ddf73c4896487f008c56e3c322cccd40c00678b9
M. Scott Ford <scott@corgibytes.com>
2023-08-30 20:15:41 -0400

    Removes imports per release notes for grpc gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-import com.google.protobuf.gradle.generateProtoTasks
 import com.google.protobuf.gradle.id
-import com.google.protobuf.gradle.plugins
 import com.google.protobuf.gradle.protobuf
-import com.google.protobuf.gradle.protoc
 import org.gradle.kotlin.dsl.invoke
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
